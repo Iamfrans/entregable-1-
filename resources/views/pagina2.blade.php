8c5435912d9136f809f208332c8531f39a5cb2e4
Iamfrans <1323544@senati.pe>
2024-05-08 11:24:41 -0500

    avance

--- resources/views/pagina2.blade.php
+++ resources/views/pagina2.blade.php
@@ -1,104 +1,210 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Página de Música</title>
-    <!-- Agregar Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <!-- Estilos personalizados -->
-    <style>
-        body {
-            background-color: #f8f9fa; /* Color de fondo */
-            color: #343a40; /* Color del texto */
-        }
-        .jumbotron {
-            background-image: url('https://source.unsplash.com/1600x900/?music'); /* Imagen de fondo */
-            background-size: cover;
-            color: #ffffff; /* Color del texto */
-        }
-        .card {
-            background-color: #ffffff; /* Color de fondo de las tarjetas */
-            border: none; /* Sin borde */
-        }
-        .card-title {
-            color: #343a40; /* Color del título de la tarjeta */
-        }
-        .card-text {
-            color: #6c757d; /* Color del texto de la tarjeta */
-        }
-        .btn-primary {
-            background-color: #007bff; /* Color de fondo del botón */
-            border-color: #007bff; /* Color del borde del botón */
-        }
-        .btn-primary:hover {
-            background-color: #0056b3; /* Cambio de color al pasar el ratón sobre el botón */
-            border-color: #0056b3; /* Cambio de color del borde al pasar el ratón sobre el botón */
-        }
-        .navbar {
-            background-color: #007bff; /* Color de fondo de la barra de navegación */
-        }
-        .navbar-brand {
-            color: #ffffff; /* Color del texto del enlace de la barra de navegación */
-        }
-        .navbar-brand:hover {
-            color: #f8f9fa; /* Cambio de color al pasar el ratón sobre el enlace de la barra de navegación */
-        }
-    </style>
-</head>
-<body>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta
+			name="viewport"
+			content="width=device-width, initial-scale=1.0"
+		/>
+		<title>Pagina Producto</title>
+		<link rel="stylesheet" href="..\resources\css\welcome.css" />
+	</head>
+	<body>
+        
+		<header>
+			<h1>ZAPATERIA FRANS EL BUENO</h1>
+		</header>
 
-    <nav class="navbar navbar-expand-lg navbar-dark">
-        <div class="container">
-            <a class="navbar-brand" href="#">Página de Música</a>
-        </div>
-    </nav>
+		<div class="container-title">Zapatos</div>
 
-    <div class="jumbotron text-center">
-        <h1 class="display-4">¡Bienvenido a tu Página de Música Favorita!</h1>
-        <p class="lead">Descubre las últimas canciones, álbumes y artistas que te encantarán.</p>
-        <hr class="my-4">
-        <p>Explora nuestra colección y disfruta de la mejor música.</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Explorar</a>
-    </div>
+		<main>
+			<div class="container-img">
+				<img
+					src="https://exclusiveshopperu.com/wp-content/uploads/Air-Jordan-1-Low-x-Travis-Scott-Black-Phantom.jpg"
+					alt="imagen-producto"
+				/>
+			</div>
+			<div class="container-info-product">
+				<div class="container-price">
+					<span>$100.00</span>
+					<i class="fa-solid fa-angle-right"></i>
+				</div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card mb-4">
-                    <img src="https://source.unsplash.com/400x300/?music,concert" class="card-img-top" alt="Concierto de Música">
-                    <div class="card-body">
-                        <h5 class="card-title">Conciertos en Vivo</h5>
-                        <p class="card-text">Descubre los próximos conciertos y festivales musicales en tu área.</p>
-                        <a href="#" class="btn btn-primary">Ver Más</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card mb-4">
-                    <img src="https://source.unsplash.com/400x300/?music,album" class="card-img-top" alt="Álbum de Música">
-                    <div class="card-body">
-                        <h5 class="card-title">Nuevos Álbumes</h5>
-                        <p class="card-text">Explora los últimos lanzamientos de álbumes de tus artistas favoritos.</p>
-                        <a href="#" class="btn btn-primary">Ver Más</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card mb-4">
-                    <img src="https://source.unsplash.com/400x300/?music,artist" class="card-img-top" alt="Artista de Música">
-                    <div class="card-body">
-                        <h5 class="card-title">Destacados Artistas</h5>
-                        <p class="card-text">Descubre más sobre tus artistas favoritos y su música.</p>
-                        <a href="#" class="btn btn-primary">Ver Más</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+				<div class="container-details-product">
+					<div class="form-group">
+						<label for="colour">Color</label>
+						<select name="colour" id="colour">
+							<option disabled selected value="">
+								Escoge una opción
+							</option>
+							<option value="rojo">Rojo</option>
+							<option value="blanco">Blanco</option>
+							<option value="beige">Beige</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="size">Talla</label>
+						<select name="size" id="size">
+							<option disabled selected value="">
+								Escoge una opción
+							</option>
+							<option value="40">40</option>
+							<option value="42">42</option>
+							<option value="43">43</option>
+							<option value="44">44</option>
+						</select>
+					</div>
+					<button class="btn-clean">Limpiar</button>
+				</div>
 
-    <!-- Agregar Bootstrap JS (opcional, solo si necesitas componentes de JavaScript) -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+				<div class="container-add-cart">
+					<div class="container-quantity">
+						<input
+							type="number"
+							placeholder="1"
+							value="1"
+							min="1"
+							class="input-quantity"
+						/>
+						<div class="btn-increment-decrement">
+							<i class="fa-solid fa-chevron-up" id="increment"></i>
+							<i class="fa-solid fa-chevron-down" id="decrement"></i>
+						</div>
+					</div>
+					<button class="btn-add-to-cart">
+						<i class="fa-solid fa-plus"></i>
+						Añadir al carrito
+					</button>
+				</div>
+
+				<div class="container-description">
+					<div class="title-description">
+						<h4>Descripción</h4>
+						<i class="fa-solid fa-chevron-down"></i>
+					</div>
+					<div class="text-description">
+						<p>
+							Lorem ipsum dolor, sit amet consectetur adipisicing
+							elit. Laboriosam iure provident atque voluptatibus
+							reiciendis quae rerum, maxime placeat enim cupiditate
+							voluptatum, temporibus quis iusto. Enim eum qui delectus
+							deleniti similique? Lorem, ipsum dolor sit amet
+							consectetur adipisicing elit. Sint autem magni earum est
+							dolorem saepe perferendis repellat ipsam laudantium cum
+							assumenda quidem quam, vero similique? Iusto officiis
+							quod blanditiis iste?
+						</p>
+					</div>
+				</div>
+
+				<div class="container-additional-information">
+					<div class="title-additional-information">
+						<h4>Información adicional</h4>
+						<i class="fa-solid fa-chevron-down"></i>
+					</div>
+					<div class="text-additional-information hidden">
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio laudantium, enim maxime asperiores consequatur officiis corrupti iste dolore, ipsam obcaecati eum pariatur veritatis minima nobis eveniet. Accusantium praesentium rem aliquam?</p>
+					</div>
+				</div>
+
+				<div class="container-reviews">
+					<div class="title-reviews">
+						<h4>Reseñas</h4>
+						<i class="fa-solid fa-chevron-down"></i>
+					</div>
+					<div class="text-reviews hidden">
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi vero eveniet consectetur fuga, vitae et quae aspernatur obcaecati culpa excepturi cum veniam distinctio dolores recusandae cupiditate error animi similique itaque?</p>
+					</div>
+				</div>
+
+				<div class="container-social">
+					<span>Compartir</span>
+					<div class="container-buttons-social">
+						<a href="#"><i class="fa-solid fa-envelope"></i></a>
+						<a href="#"><i class="fa-brands fa-facebook"></i></a>
+						<a href="#"><i class="fa-brands fa-twitter"></i></a>
+						<a href="#"><i class="fa-brands fa-instagram"></i></a>
+						<a href="#"><i class="fa-brands fa-pinterest"></i></a>
+					</div>
+				</div>
+			</div>
+		</main>
+
+		<section class="container-related-products">
+			<h2>Productos Relacionados</h2>
+			<div class="card-list-products">
+				<div class="card">
+					<div class="card-img">
+						<img
+							src="https://media.revistagq.com/photos/650bfa46330e4dc6722c7d05/16:9/w_1920,c_limit/Nike-Dunk-Low-SP-University-Red.jpg"
+							alt="producto-1"
+						/>
+					</div>
+					<div class="info-card">
+						<div class="text-product">
+							<h3>Nike Dunk Low University Red</h3>
+							<p class="category">Footwear, Sneakers</p>
+						</div>
+						<div class="price">$150.00</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-img">
+						<img
+							src="https://exclusiveshopperu.com/wp-content/uploads/2019/04/Jordan-Retro-1-High-OG-1.jpg"
+							alt="producto-2"
+						/>
+					</div>
+					<div class="info-card">
+						<div class="text-product">
+							<h3>Air Jordan 1 Retro High Sports Illustrated</h3>
+							<p class="category">Footwear, Sneakers</p>
+						</div>
+						<div class="price">$200.00</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-img">
+						<img
+							src="https://www.badtoro.es/wp-content/uploads/2023/01/zapatillas-bad-bunny-101wiq-1.jpg"
+							alt="producto-3"
+						/>
+					</div>
+					<div class="info-card">
+						<div class="text-product">
+							<h3>Adidas Forum Bad Bunny "The First Café"</h3>
+							<p class="category">Footwear, Sneakers</p>
+						</div>
+						<div class="price">$300.00</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-img">
+						<img
+							src="https://rukminim2.flixcart.com/image/850/1000/xif0q/shoe/l/i/d/-original-imagtbteehhvvkfg.jpeg?q=90&crop=false"
+							alt="producto-4"
+						/>
+					</div>
+					<div class="info-card">
+						<div class="text-product">
+							<h3>New Balance 550 Sneakers For Men</h3>
+							<p class="category">Footwear, Sneakers</p>
+						</div>
+						<div class="price">$400.00</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<footer>
+			<p></p>
+		</footer>
+
+		<script src="https://kit.fontawesome.com/daaa9c6519.js" crossorigin="anonymous"></script>
+    <script src="..\resources\js\welcome.js"></script>
 </body>
 </html>
+
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,62 +1,113 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Menú de Navegación</title>
+    <title>Tienda de Zapatillas Urbanas</title>
     <!-- Agregar Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Estilos personalizados -->
     <style>
-        /* Estilos personalizados */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8f9fa; /* Color de fondo */
+            color: #343a40; /* Color del texto */
+        }
         .navbar {
-            background-color: #6C5B7B; /* Color de fondo */
+            background-color: #000;
+            border-bottom: 1px solid #1a1a1a;
         }
-        .navbar-brand, .nav-link {
-            color: #F8B195 !important; /* Color del texto */
+        .navbar-brand, .navbar-nav .nav-link {
+            color: #fff;
+            font-weight: bold;
+            text-transform: uppercase;
         }
-        .navbar-brand:hover, .nav-link:hover {
-            color: #FFFFFF !important; /* Cambio de color al pasar el ratón */
+        .navbar-nav .nav-link {
+            padding: 0.5rem 1rem;
+        }
+        .navbar-nav .nav-link:hover {
+            color: #ffc107;
+        }
+        .carousel-item img {
+            width: 100%;
+            height: auto;
+        }
+        .jumbotron {
+            background-image: url('https://source.unsplash.com/1600x900/?sneakers,promotion'); /* Imagen de fondo */
+            background-size: cover;
+            color: #ffffff; /* Color del texto */
+            text-align: center;
+        }
+        .jumbotron h1, .jumbotron p {
+            color: #ffffff; /* Color del texto */
         }
     </style>
 </head>
 <body>
     <!-- Menú de navegación -->
-    <nav class="navbar navbar-expand-lg navbar-dark">
+    <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <a class="navbar-brand" href="#">Menú de Navegación</a>
+            <a class="navbar-brand" href="#">Zapatillas Urbanas</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <i class="fas fa-bars" style="color: #fff;"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="http://localhost/frans/public/pagina1">Página 1</a>
+                        <a class="nav-link" href="#">Inicio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="http://localhost/frans/public/pagina2">Página 2</a>
+                        <a class="nav-link" href="#">Catálogo</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="http://localhost/frans/public/pagina3">Página 3</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="http://localhost/frans/public/pagina4">Página 4</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="http://localhost/frans/public/pagina5">Página 5</a>
+                        <a class="nav-link" href="#">Contacto</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
 
+    <!-- Carrusel de zapatillas -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="3000"> <!-- Cambio aquí -->
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="https://images.unsplash.com/photo-1587855049254-351f4e55fe2a?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="Promoción 1">
+        </div>
+        <div class="carousel-item">
+            <img src="https://images.unsplash.com/photo-1641745900309-75ceed0153e1?q=80&w=2028&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="Promoción 2">
+        </div>
+        <div class="carousel-item">
+            <img src="https://images.unsplash.com/photo-1617906641165-c7d0117c18ca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="Promoción 3">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Siguiente</span>
+    </a>
+</div>
+
+
+    
     <!-- Contenido de la página -->
-    <div class="container mt-4">
-        <h1>Contenido de la página</h1>
-        <p>Aquí puedes agregar el contenido de tu página.</p>
+    <div class="jumbotron">
+        <h1 class="display-4">¡Bienvenido a tu Tienda de Zapatillas Favorita!</h1>
+        <p class="lead">Descubre las últimas promociones, modelos exclusivos y más.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Explorar</a>
     </div>
 
     <!-- Agregar Bootstrap JS (opcional, solo si necesitas componentes de JavaScript) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
